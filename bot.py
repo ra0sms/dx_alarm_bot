3b73ad985c78ccf166ca6876419d9231076d0154
ra0sms <ra0sms@bk.ru>
2024-10-15 21:01:55 +0800

    add sending message

--- bot.py
+++ bot.py
@@ -4,34 +4,38 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+import random
 from config_reader import config
 
-# Включаем логирование, чтобы не пропустить важные сообщения
+# Включаем логирование
 logging.basicConfig(level=logging.INFO)
 # Объект бота
 bot = Bot(token=config.bot_token.get_secret_value())
 # Диспетчер
 dp = Dispatcher()
 
+
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+    await message.answer(f"Hello {message.from_user.full_name}!")
 
-# Хэндлер на команду /test1
-@dp.message(Command("test1"))
+# Хэндлер на команду /rand
+@dp.message(Command("rand"))
 async def cmd_test1(message: types.Message):
-    await message.reply("Test 1")
+    await message.answer(f"Случайное число: {random.randint(0,100)}")
 
 # Хэндлер на команду /test2
 async def cmd_test2(message: types.Message):
     await message.reply("Test 2")
 
-# Запуск процесса поллинга новых апдейтов
+
+#----------------Main---------------------------- 
 async def main():
     dp.message.register(cmd_test2, Command("test2"))
-    dp.message.register(cmd_test1, Command("test1"))
+    dp.message.register(cmd_test1, Command("rand"))
     dp.message.register(cmd_start, Command("start"))
+    await bot.send_message(591915735,text="Перезапуск")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
