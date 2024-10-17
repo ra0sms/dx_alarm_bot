67b1a5c1dd1720b2b64180c204c216fa629de8d2
ra0sms <ra0sms@bk.ru>
2024-10-17 20:11:10 +0800

    workable prototype

--- bot.py
+++ bot.py
@@ -3,36 +3,86 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters.command import Command
+from aiogram.filters.command import Command, CommandObject
 import random
+import subprocess
 from config_reader import config
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(token=config.bot_token.get_secret_value())
 dp = Dispatcher()
 
+@dp.message(Command("get"))
+async def cmd_get(
+        message: types.Message,
+        command: CommandObject
+):
+    spots = ""
+    if command.args is None:
+        await message.answer(
+            "Ошибка: не переданы аргументы"
+        )
+        return
+    try:
+        callsign = command.args
+    except ValueError:
+        await message.answer(
+            "Ошибка: неправильный формат команды. Пример:\n"
+            "/get <callsign>"
+        )
+        return
+    spots = read_last_spots(callsign)
+    await message.answer(
+        f"DX: {callsign.upper()}\n"
+        "Последние споты: \n"
+        f"{spots}"
+    )
+
+
+
 
 @dp.message(Command("start"))
 async def cmd_start(message: types.Message):
-    await message.answer(f"Hello {message.from_user.full_name}!")
+    await message.answer(
+        f"Hello {message.from_user.full_name}!\n"
+        "Споты за последние 15 минут: /get <позывной>\n"
+        "Например, /get ra0sms"        
+    )
 
 
 @dp.message(Command("rand"))
-async def cmd_test1(message: types.Message):
+async def cmd_rand(message: types.Message):
     await message.answer(f"Случайное число: {random.randint(0,100)}")
 
 
-async def cmd_test2(message: types.Message):
-    await message.reply("Test 2")
+
+def read_last_spots(callsign: str) -> str:
+    try:
+        subprocess.run(["parse/get_spots.sh"] + [callsign])
+        file_path = f'parse/dx_spots/{callsign}.txt'
+    except Exception as e:
+        return "Нет файла с dx-позывным"
+    out =""
+
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    for line in lines:
+        out += (line)
+    if out:
+        return out
+    else:
+        return "Нет спотов"
 
 
 #----------------Main---------------------------- 
 async def main():
-    dp.message.register(cmd_test2, Command("test2"))
-    dp.message.register(cmd_test1, Command("rand"))
+    dp.message.register(cmd_rand, Command("rand"))
     dp.message.register(cmd_start, Command("start"))
+    dp.message.register(cmd_get, Command("get"))
     await bot.send_message(591915735,text="Перезапуск")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
--- parse/parse_adif.py
+++ parse/parse_adif.py
@@ -4,41 +4,50 @@ from datetime import datetime,timezone
 import sys
 
 callsign = sys.argv[1]  
+
+def cut_to_dot(text: str)->str:
+    index = text.find('.')
+
+    if index != -1:  # Проверяем, что точка найдена
+        result = text[:index]
+    else:
+        result = text  # Если точки нет, возвращаем всю строку
+
+    return(result)  # Вывод: Это пример
+
+
+
   
-def parse_adif_to_txt():  
+def parse_adif_to_txt():
+    prev_band_value = "1"  
+    band_value = "1"
     with open(f'parse/dx_spots/{callsign}.adif') as inp_file:  
         with open(f'parse/dx_spots/{callsign}.txt', 'w') as out_file:  
             for line in inp_file:  
                 qso_date = line.find('QSO_DATE:')  
                 if qso_date == -1:  
                     continue  
+                prev_band_value = band_value
                 qso_date_value = line[line.find('>', qso_date) + 1 : line.find('<', qso_date)]  
                 time_on = line.find('TIME_ON:')  
-                time_on_value = line[line.find('>', time_on) + 1 : line.find('<', time_on)]  
+                time_on_value = line[line.find('>', time_on) + 1 : line.find('<', time_on) -2 ]  
                 band = line.find('FREQ:')  
                 band_value = line[line.find('>', band) + 1 : line.find('<', band)]  
                 mode = line.find('MODE:')  
                 mode_value = line[line.find('>', mode) + 1 : line.find('<', mode)]  
-                call = line.find('CALL:')  
-                call_value = line[line.find('>', call) + 1 : line.find('<', call)]  
-                rst_send = line.find('APP_PSKREP_SNR')  
-                rst_send_value = line[line.find('>', rst_send) + 1 : line.find('<', rst_send)]  
+                #call = line.find('CALL:')  
+                #call_value = line[line.find('>', call) + 1 : line.find('<', call)]  
+                #rst_send = line.find('APP_PSKREP_SNR')  
+                #rst_send_value = line[line.find('>', rst_send) + 1 : line.find('<', rst_send)]  
+                if int(cut_to_dot(band_value)) != int(cut_to_dot(prev_band_value)):
+                    result = qso_date_value + ' ' + time_on_value + ' ' + band_value + ' ' + mode_value + ' '
+                    print(result)  
+                    out_file.write(result + '\n')  
   
-                result = qso_date_value + ' ' + time_on_value + ' ' + band_value + ' ' + mode_value + ' ' + call_value + ' ' + rst_send_value  
-                print(result)  
-                out_file.write(result + '\n')  
-  
-  
-def get_current_time():  
-    now = datetime.now(timezone.utc)  
-    current_date = now.strftime("%Y%m%d")  
-    current_time = now.strftime("%H%M%S")  
-    print(current_date + " " + current_time)  
   
   
 def main():  
-    parse_adif_to_txt()  
-    get_current_time()  
+    parse_adif_to_txt()   
   
   
 if __name__ == "__main__":  
