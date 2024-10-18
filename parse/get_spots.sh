c43a52320d2f2717c0382e599d2edb242acde101
ra0sms <ra0sms@bk.ru>
2024-10-18 08:59:03 +0800

    add slash calls

--- parse/get_spots.sh
+++ parse/get_spots.sh
@@ -2,5 +2,7 @@
 
 HTTP="https://pskreporter.info/cgi-bin/pskdata.pl?&adif=1&days=0.01&senderCallsign="
 url=`echo $HTTP$1`
-curl --output parse/dx_spots/$1.adif "$url"
-parse/parse_adif.py $1
+name=$1
+call=`echo ${name////_}`
+curl --output parse/dx_spots/$call.adif "$url"
+parse/parse_adif.py $call
